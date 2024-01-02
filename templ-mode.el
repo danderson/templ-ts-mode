e50674b89ac1bd07e9a9093157095e29ce6a5d09
David Anderson <dave@natulte.net>
2024-01-02 12:46:22 -0800

    More hacking, attempt at using go subparser

--- templ-mode.el
+++ templ-mode.el
@@ -138,6 +138,110 @@
 
      )))
 
+(defun templ-ts--notch-range (range notch)
+  "Return the portions of RANGE on either side of NOTCH.
+RANGE, NOTCH are both (start . end) cons pairs.  The return value
+is a list of 2 ranges in the same format, giving the portions of
+RANGE that fall before and after NOTCH.  Both values can be nil
+if NOTCH overlaps one or both ends of RANGE."
+  (let* ((range-start (car range))
+         (range-end (cdr range))
+         (notch-start (min (max (car notch) range-start)
+                           range-end))
+         (notch-end (max (min (cdr notch) range-end)
+                         range-start))
+         (rem-start (if (equal range-start notch-start)
+                        nil
+                      (cons range-start notch-start)))
+         (rem-end (if (equal range-end notch-end)
+                      nil
+                    (cons notch-end range-end))))
+    (list rem-start rem-end)))
+
+(ert-deftest templ-ts--test-notch-range ()
+  "Tests the range notching function."
+  (should (equal (templ-ts--notch-range '(100 . 200) '(0 . 50))
+                 '(nil (100 . 200))))
+  (should (equal (templ-ts--notch-range '(100 . 200) '(0 . 300))
+                 '(nil nil)))
+  (should (equal (templ-ts--notch-range '(100 . 200) '(50 . 150))
+                 '(nil (150 . 200))))
+  (should (equal (templ-ts--notch-range '(100 . 200) '(150 . 250))
+                 '((100 . 150) nil)))
+  (should (equal (templ-ts--notch-range '(100 . 200) '(250 . 300))
+                 '((100 . 200) nil)))
+  (should (equal (templ-ts--notch-range '(100 . 200) '(120 . 180))
+                 '((100 . 120) (180 . 200))))
+  )
+
+(defun templ-ts--accumulate-notch (acc notch)
+  "Notch the first element of ACC with NOTCH.
+Places the results of the notching back onto the front of ACC.
+Intended for use in a seq-reduction."
+  (let* ((range (car acc))
+         (rest (cdr acc))
+         (split (templ-ts--notch-range range notch))
+         (clean (nreverse (seq-filter #'identity split))))
+    (append clean rest)))
+
+(defun templ-ts--invert-source-ranges (ranges start end)
+  ""
+  ; Empirically, treesit-query-range returns ranges in order of
+  ; appearance in the buffer. However, the documentation doesn't
+  ; guarantee that behavior. Defensively sort, just in case.
+  (sort ranges (lambda (r1 r2) (< (car r1) (car r2))))
+  (let* ((init-range (cons start end))
+         (reduction (seq-reduce #'templ-ts--accumulate-notch
+                                ranges
+                                (list init-range))))
+    (nreverse reduction)))
+
+(defun templ-ts--range-is-whitespace (range)
+  ""
+  (string-blank-p (buffer-substring-no-properties (car range) (cdr range))))
+
+(defun templ-ts--range-length (range)
+  ""
+  (- (cdr range) (car range)))
+
+(defun templ-ts--remove-whitespace (ranges)
+  ""
+  (seq-filter (lambda (range)
+                (or (> (templ-ts--range-length range) 10)
+                    (not (templ-ts--range-is-whitespace range))))
+              ranges))
+
+(defun templ-ts--top-level-templ-ranges (start end)
+  ""
+  (let ((query '((source_file [(css_declaration)
+                                (script_declaration)
+                                (component_declaration)] @templ))))
+    (treesit-query-range 'templ query start end)))
+
+(defun templ-ts--top-level-go-ranges (start end)
+  ""
+  (interactive)
+  (templ-ts--remove-whitespace
+   (templ-ts--invert-source-ranges (templ-ts--top-level-templ-ranges start end)
+                                   start end)))
+
+(defun templ-ts--set-go-ranges (start end)
+  ""
+  (let ((ranges (templ-ts--top-level-go-ranges (point-min) (point-max)))
+         (parser (treesit-parser-create 'go)))
+    (treesit-parser-set-included-ranges parser ranges)))
+
+(defvar templ-ts--range-rules
+  (list
+
+   #'templ-ts--set-go-ranges
+
+   ;; :embed 'go
+   ;; :host 'templ
+   ;; '((element (expression "{" (_) @capture "}")))
+
+   ))
+
 (defun templ-ts-setup ()
   (interactive)
   (setq-local treesit-font-lock-feature-list
@@ -147,13 +251,19 @@
                 bracket delimiter function operator property variable)))
 
   (setq-local treesit-font-lock-settings
-              (apply #'treesit-font-lock-rules
-                     (append templ-ts--go-font-lock-rules
+              (append go-ts-mode--font-lock-settings
+                      (apply #'treesit-font-lock-rules
                              templ-ts--templ-font-lock-rules)))
 
   (setq-local treesit-simple-indent-rules templ-ts--indent-rules)
 
+  (setq-local treesit-range-settings
+              (apply #'treesit-range-rules templ-ts--range-rules))
+
   (treesit-major-mode-setup))
+
+(setq combobulate-rules-templ '())
+(setq combobulate-rules-templ-inverted '())
 
 (define-derived-mode templ-ts-mode go-ts-mode "Templ"
   "Major mode for editing Templ files."
@@ -224,3 +334,31 @@
           font-lock-misc-punctuation-face
           )
         ))
+
+(defvar ultravomit-parser-overlays nil)
+
+(defun ultravomit-subparsers ()
+  ""
+  (interactive)
+  (dolist (parser (treesit-parser-list))
+    (let ((language (treesit-parser-language parser))
+          (ranges (treesit-parser-included-ranges parser))
+          (overlays nil))
+      (dolist (range ranges)
+        (let ((overlay (make-overlay (car range) (cdr range))))
+          (overlay-put overlay 'face '(:background "#330000"))
+          (push overlay overlays)))
+      (dolist (overlay (plist-get ultravomit-parser-overlays language))
+        (delete-overlay overlay))
+      (setq-local ultravomit-parser-overlays (plist-put ultravomit-parser-overlays language overlays)))))
+
+(defun ultravomit-subparsers-clear ()
+  ""
+  (interactive)
+  (let ((alist (seq-partition ultravomit-parser-overlays 2)))
+    (dolist (language alist)
+      (dolist (overlay (cadr language))
+        (delete-overlay overlay))))
+  (setq-local ultravomit-parser-overlays nil))
+
+(provide 'templ-mode)
