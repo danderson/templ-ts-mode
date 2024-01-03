07a8b6a615a0fa85af3d87aae549d69bceeda5ac
David Anderson <dave@natulte.net>
2024-01-03 12:05:07 -0800

    Add a small commentary to the major mode.

--- templ-ts-mode.el
+++ templ-ts-mode.el
@@ -26,7 +26,11 @@
 
 ;;; Commentary:
 
-;;
+;; This package provides syntax highlighting and indentation for Templ
+;; files.  To use the `templ-ts-mode` major mode, the Templ and
+;; Javascript tree-sitter grammars must be installed.  For more
+;; information about Templ's syntax and use, visit the Templ website
+;; at https://templ.guide .
 
 ;;; Code:
 
